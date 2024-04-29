fe6a4b8cb6c946b166c079a8167745bfc94fa324
c10aking <grkim43@gmail.com>
2024-04-29 14:56:41 +0900

    04/29

--- prac03.py
+++ prac03.py
@@ -10,5 +10,3 @@ from math import *
 print(floor(4.99)) # 내림 4
 print(ceil(3.14)) # 올림 4
 print(sqrt(16)) # 제곱근 4
-
-
---
+++ prac09.py
@@ -0,0 +1,10 @@
+weather = '맑음'
+# if 조건:
+#     실행 명령문
+
+if weather == '비':
+    print('우산을 챙기세요')
+elif weather == '미세먼지':
+    print('마스크를 잘 챙기세요')
+else:
+    print('준비물 필요없어요')
